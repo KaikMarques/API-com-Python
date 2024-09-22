907b63ef11d051b95a483f2ffb65196689af3843
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-22 17:58:47 -0300

    dd criar

--- app.py
+++ app.py
@@ -20,11 +20,34 @@ livros = [
   }
 ]
 
-# Concultar(todos)
-@app.route('/livros')
+# Consultar(todos)
+@app.route('/livros', methods=['GET'])
 def obter_livros():
   return jsonify(livros)
+
 # Concultar(id)
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livro_por_id(id):
+  for livro in livros:
+    if livro.get('id') == id: #verificando se o id que foi passado é o mesmo que o solicitado
+      return jsonify(livro)
+    
 # Editar
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+  livro_alterado = request.get_json()
+  for indice,livro in enumerate(livros):
+    if livro.get('id') == id:
+      livros[indice].update(livro_alterado)
+      return jsonify(livros[indice])
+
+# Criar
+@app.route('/livros',methods=['POST'])
+def incluir_novo_livro():
+  novo_livro = request.get_json()
+  livros.append(novo_livro)
+  return jsonify(livros[indice])
+
 # Excluir
+
 app.run(port=5000,host='localhost',debug=True)
